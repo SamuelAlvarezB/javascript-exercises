8b2e2cf6b1c0ec7b4c2b43d5a14fc6f6fb673b92
Samuel Alvarez <abrena2@gmail.com>
2025-09-19 13:32:31 +0200

    Solve exercise 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    let newArray = [a, b];
+    if (a < 0 || b < 0 || a % 1 != 0 || b % 1 != 0 || typeof (a) != "number" || typeof (b) != "number") {
+        return "ERROR";
+    }
+    if (a < b) {
+        let count = a + 1;
+        while (count < b) {
+            newArray.push(count);
+            count++;
+        }
+    } else if (b < a) {
+        let count = b + 1;
+        while (count < a) {
+            newArray.push(count);
+            count++;
+        }
+    }
 
+    return newArray.reduce((total, value) => total + value);
 };
+
+console.log(sumAll(4, 1));
 
 // Do not edit below this line
 module.exports = sumAll;
