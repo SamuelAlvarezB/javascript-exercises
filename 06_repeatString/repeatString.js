6fd38d05ede3fea2e6e99fa50721ef2b2476074a
Samuel Alvarez <abrena2@gmail.com>
2025-09-19 12:18:57 +0200

    Solve exercise 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, iterations) {
+    let repeatedString = "";
+    if (iterations < 0) {
+        repeatedString = "ERROR";
+    } else {
+        for (let i = 0; i < iterations; i++) {
+            repeatedString += string;
+        }
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
