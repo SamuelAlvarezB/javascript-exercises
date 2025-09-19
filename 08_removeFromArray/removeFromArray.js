1d716e7d085f8a52553c4c13a84b63490ea1233f
Samuel Alvarez <abrena2@gmail.com>
2025-09-19 13:03:47 +0200

    Solve exercise 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...otherArgs) {
+    for (let arg of otherArgs) {
+        if (array.includes(arg)) {
+            while (array.includes(arg)) {
+                array.splice(array.findIndex((element) => element === arg), 1);
+            }
+        }
+    };
+    return array;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
