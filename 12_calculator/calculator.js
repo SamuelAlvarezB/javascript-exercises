75f6fb58d4ba7870a0744d14ba37a35ed05e0fb7
Samuel Alvarez <abrena2@gmail.com>
2025-09-29 18:44:59 +0200

    Solve exercise 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((total, currentValue) => total + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((total, currentValue) => total * currentValue, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  let result = 1;
+  if (a != 0 && a != 1) {
+    let numArray = [];
+    for (let i = 1; i <= a; i++) {
+      numArray.push(i);
+    }
+    result = numArray.reduce((total, currentValue) => total * currentValue, 1);
+  }
+  return result;
 };
 
 // Do not edit below this line
