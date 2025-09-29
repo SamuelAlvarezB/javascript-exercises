ca5e3582555e04c84cc0cf39d087b80469b825ef
Samuel Alvarez <abrena2@gmail.com>
2025-09-29 20:25:12 +0200

    Solve exercise 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    for (person of people) {
+        if (person.yearOfDeath === undefined) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+    }
+    return people.sort((a, b) => (a.yearOfDeath - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth))[people.length - 1];
 };
 
 // Do not edit below this line
