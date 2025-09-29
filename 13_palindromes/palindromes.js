62de05dbf95ee3a440428cc9d9d54271c775c8ef
Samuel Alvarez <abrena2@gmail.com>
2025-09-29 19:17:18 +0200

    SOlve exercise 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string = "") {
+    let originalString = string.toLowerCase();
+    originalString = originalString.replaceAll(" ", "");
+    originalString = originalString.replaceAll(",", "");
+    originalString = originalString.replaceAll("!", "");
+    originalString = originalString.replaceAll(".", "");
+    let reverseString = originalString.split("").reverse();
+    reverseString = reverseString.join("");
+    if (originalString === reverseString) {
+        return true;
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
