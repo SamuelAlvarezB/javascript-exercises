cbe562efc270f1d22cff65bf88c2338e11fbc7db
Samuel Alvarez <abrena2@gmail.com>
2025-09-29 19:34:02 +0200

    Solve exercise 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+    let fibonacciSequence = [1, 1];
+    num = Number.parseInt(num);
+    if (num === 0) {
+        return 0;
+    } else if (num < 0) {
+        return "OOPS";
+    } else {
+        for (let i = 2; i < num; i++) {
+            fibonacciSequence.push(fibonacciSequence[i - 1] + fibonacciSequence[i - 2]);
+        }
+        return fibonacciSequence[num - 1];
+    }
 };
 
 // Do not edit below this line
